8f642f1f49589b418d48cab0d80578a1486f3a86
Jassim Mougamadou <80199377+MGMDJassim@users.noreply.github.com>
2026-01-11 16:46:29 +0100

    push

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Tableaux tab = new Tableaux();
+
+        int num = tab.getcol();
+        System.out.println(num);
     }
 }
---
+++ src/Tableaux.java
@@ -0,0 +1,17 @@
+public class Tableaux{
+    private int COLUMN;
+    private int ROW;
+
+    public Tableaux(){
+        this.COLUMN = 8;
+        this.ROW = 9;
+    }
+
+    public int getcol(){
+        return COLUMN;
+    }
+
+    public int getrow(){
+        return ROW;
+    }
+}
