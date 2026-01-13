56fd800085dc062f9d743c9102391dfb55b153ad
Jassim <jassim91@outlook.fr>
2026-01-13 23:44:57 +0100

    changement

--- src/App.java
+++ src/App.java
@@ -1,9 +1,25 @@
-import Model.Plateau;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-public class App {
-    public static void main(String[] args) throws Exception {
-        Plateau pl = new Plateau();
-        pl.printTableau();
+public class App extends Application {
 
+    @Override
+    public void start(Stage stage) {
+        Button button = new Button("Clique moi");
+        button.setOnAction(e -> System.out.println("Bouton cliqué !"));
+
+        StackPane root = new StackPane(button);
+        Scene scene = new Scene(root, 400, 300);
+
+        stage.setTitle("JavaFX sans FXML");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
